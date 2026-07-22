6ad547526ad020d08343a232bd79e00ddd8c2f17
Dawid Uniowski <dawid.uniowski@gmail.com>
2026-07-22 12:54:56 +0200

    clean up

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  envPrefix: ["VITE_", "REACT_APP_"],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./src/setupTests.ts",
-  },
 });
